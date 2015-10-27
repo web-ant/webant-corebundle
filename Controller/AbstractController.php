09c837d54a5202e9b3bc7b60f1c81ecff4b98f0b
white1code <white1code@gmail.com>
2015-10-27 16:43:53 +0300

    fix bugs in usertocken

--- Controller/AbstractController.php
+++ Controller/AbstractController.php
@@ -435,7 +435,7 @@ abstract class AbstractController extends FOSRestController
             }
         } catch
         (\Exception $e) {
-            throw new HttpException(400, 'Error with Database ' . $e->getMessage());
+            throw new HttpException(40, 'Error with Database ' . $e->getMessage());
         }
 
         return $object;
@@ -485,8 +485,12 @@ abstract class AbstractController extends FOSRestController
                     }
 
                     $prop->setValue($object, $subObject);
-                } else if(!is_null($value))
+                } else if(!is_null($value)) {
+                    if($type == '/DateTime') {
+                        $value = new \DateTime($value);
+                    }
                     $prop->setValue($object, $value);
+                }
             }
         }
         //запуск валидатора
--- EventListener/ListenerController.php
+++ EventListener/ListenerController.php
@@ -5,41 +5,32 @@
  * Date: 19.10.15
  * Time: 11:50
  */
-
 namespace WebAnt\CoreBundle\EventListener;
-
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use WebAnt\CoreBundle\Entity\QueryLogs;
 use Doctrine\ORM\EntityManager;
-
 class ListenerController
 {
     protected $token_storage;
-
     public function __construct(TokenStorageInterface $token_storage, EntityManager $em)
     {
         $this->token_storage = $token_storage;
         $this->em = $em;
     }
-
     public function onKernelException(GetResponseForExceptionEvent $event)
     {
         $event->getRequest()->attributes->set('exception', $event->getException());
     }
-
     public function onKernelResponse(FilterResponseEvent $event)
     {
         $controller = explode('\\', $event->getRequest()->attributes->get('_controller'));
         if($controller[0] == 'WebAnt') {
             $this->addLogs($event);
         }
-
     }
-
-    private function addLogs($event,$status = true) {
-
+    private function addLogs($event) {
         $queryLogs = new QueryLogs();
         $request = $event->getRequest();
         $controller = explode('::', $request->attributes->get('_controller'));
@@ -47,18 +38,12 @@ class ListenerController
         if($user != "anon."){
             $queryLogs->setUser($user);
         }
-
         $queryLogs->setController($controller[0]);
         $queryLogs->setAction($controller[1]);
-
         $datalogs['http_code_response'] = $event->getResponse()->getStatusCode();
-
         $queryLogs->setHttpCodeResponse($datalogs['http_code_response']);
         $queryLogs->setMethod($request->getMethod());
-
         $queryLogs->setRequestTime($request->server->get('REQUEST_TIME'));
-
-
         $this->em->persist($queryLogs);
         $this->em->flush();
     }
